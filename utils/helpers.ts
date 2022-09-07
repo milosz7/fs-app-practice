fa653c06507f6ea719cec0667d499a9b572fe1d6
Milosz B <miloszbargiel@gmail.com>
2022-09-07 21:51:36 +0200

    Add schema validation helper functions

---
+++ utils/helpers.ts
@@ -0,0 +1,10 @@
+export const validateUsername = (username: string) => {
+  const usernameRegexp = /([A-Z]|[a-z]|[0-9]|[._-])\w+/;
+  return usernameRegexp.test(username);
+};
+
+export const validatePhoneNumber = (number: string) => {
+  const phoneNumberRegexp =
+    /(?:(?:(?:\+|00)?48)|(?:\(\+?48\)))?(?:1[2-8]|2[2-69]|3[2-49]|4[1-8]|5[0-9]|6[0-35-9]|[7-8][1-9]|9[145])\d{7}/;
+  return phoneNumberRegexp.test(number);
+};
