946658b527675d3d5408ba8215e2655d4d740219
Milosz B <miloszbargiel@gmail.com>
2022-09-07 21:53:23 +0200

    Add path for future client side and use connectToDb function

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { connectToDb } from './db-connect';
 
 const app = express();
+
+connectToDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 app.listen(8000, () => {
   console.log('Listening on port 8000.');
