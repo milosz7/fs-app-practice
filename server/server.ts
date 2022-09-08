dd79deba98bc058a3daa56d247095de77d7ca393
Milosz B <miloszbargiel@gmail.com>
2022-09-08 15:09:45 +0200

    Add error handling middleware and auth routes

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import path from 'path';
 import { connectToDb } from './db-connect';
+import authRoutes from './routes/auth.routes';
 
 const app = express();
 
@@ -11,7 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: 'http://localhost:3000' }));
 
+app.use('/auth', authRoutes);
 app.use('/', express.static(path.join(__dirname, '../client/build')));
+
+app.use(
+  (err: { status: number; message: string }, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    if (err) {
+      return res.status(err.status).json({ message: err.message });
+    }
+  }
+);
+
+app.use((req, res) => {
+  return res.status(404).send({ message: 'Not found.' });
+});
 
 app.listen(8000, () => {
   console.log('Listening on port 8000.');
