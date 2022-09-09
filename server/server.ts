05fee80d2da38ec6291d1d20720da3b9f2df41b7
Milosz B <miloszbargiel@gmail.com>
2022-09-09 11:19:25 +0200

    Setup session and cors middleware

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import path from 'path';
 import { connectToDb } from './db-connect';
 import authRoutes from './routes/auth.routes';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -10,14 +13,32 @@ connectToDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+}
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({ client: mongoose.connection.getClient() }),
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  })
+);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: 'http://localhost:3000' }));
+}
 
 app.use('/auth', authRoutes);
 app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 app.use(
   (err: { status: number; message: string }, req: Request, res: Response, next: NextFunction) => {
-    console.log(err);
     if (err) {
       return res.status(err.status).json({ message: err.message });
     }
