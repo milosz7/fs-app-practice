a471405788a184c7e499de5b483c105801357249
Milosz B <miloszbargiel@gmail.com>
2022-09-09 11:18:13 +0200

    Add request session type definitons and install connect mongo

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -1,8 +1,21 @@
+import mongoose from 'mongoose';
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DB_DEV_CONN_STR: string;
       DB_PROD_CONN_STR: string;
+      SESSION_SECRET: string | string[];
+    }
+  }
+  namespace Express {
+    interface Request {
+      session: {
+        user: {
+          username: string,
+          id: mongoose.Types.ObjectId
+        }
+      }
     }
   }
 }
