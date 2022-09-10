5c614df8e6264c7eb1dabc3f98bad4c9be0dd40c
Milosz B <miloszbargiel@gmail.com>
2022-09-10 17:36:00 +0200

    Add ads methods calls in ads routes with proper middlewares

--- server/routes/ads.routes.ts
+++ server/routes/ads.routes.ts
@@ -1,16 +1,18 @@
-import express from 'express'
+import express from 'express';
 import adsMethods from '../methods/ads.methods';
+import { upload } from '../utils/upload';
+import { isLoggedIn } from '../utils/authentication-middleware';
 
 const router = express.Router();
 
-router.route('/ads').get();
+router.route('/ads').get(adsMethods.getAll);
 
-router.route('/ads/:id').get();
+router.route('/ads/:id').get(adsMethods.getById);
 
-router.route('/ads').post();
+router.route('/ads').post(isLoggedIn, upload.single('image'), adsMethods.addNew);
 
-router.route('/ads/:id').delete();
+router.route('/ads/:id').delete(isLoggedIn, adsMethods.delete);
 
-router.route('/ads/:id').put();
+router.route('/ads/:id').put(isLoggedIn, upload.single('image'), adsMethods.edit);
 
 export default router;
