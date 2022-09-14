c4f438469830304cc9d1d27a22cbf136856c7ee2
Milosz B <miloszbargiel@gmail.com>
2022-09-14 19:57:19 +0200

    Install mui icons, improve router implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,16 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-import Home from './Components/Pages/Home/Home';
-import Login from './Components/Pages/Login/Login';
-import Edit from './Components/Pages/Edit/Edit';
-import Ad from './Components/Pages/Ad/Ad';
-import Add from './Components/Pages/Add/Add';
-import Register from './Components/Pages/Register/Register';
-import NotFound from './Components/Pages/NotFound/NotFound';
+import Navigation from './Components/Views/Navigation/Navigation';
+import Container from '@mui/material/Container';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/ad/:id" element={<Ad />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/*" element={<NotFound />} />
-      </>
-    )
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
   );
-
-  return <RouterProvider router={router} />;
 };
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
+import Login from './Components/Pages/Login/Login';
+import Edit from './Components/Pages/Edit/Edit';
+import Ad from './Components/Pages/Ad/Ad';
+import Add from './Components/Pages/Add/Add';
+import Register from './Components/Pages/Register/Register';
+import NotFound from './Components/Pages/NotFound/NotFound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="edit/:id" element={<Edit />} />
+      <Route path="ad/:id" element={<Ad />} />
+      <Route path="add" element={<Add />} />
+      <Route path="register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-      <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
