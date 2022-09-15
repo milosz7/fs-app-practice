6d3ab9c305788bbc26d260071a48157f49ac0cf8
Milosz B <miloszbargiel@gmail.com>
2022-09-15 13:54:23 +0200

    Make fetched Ads data populated with seller information

--- server/controllers/ads/getAll.ts
+++ server/controllers/ads/getAll.ts
@@ -1,20 +1,26 @@
-import { Request, Response } from "express";
-import { NextError } from "../../../types/declaration";
-import Ad from "../../models/Ad.model";
-import { createSearchQuery } from "../../utils/createSearchQuery";
+import { Request, Response } from 'express';
+import { NextError } from '../../../interfaces/NextError';
+import Ad from '../../models/Ad.model';
+import { createSearchQuery } from '../../utils/createSearchQuery';
+import { AdData } from '../../../interfaces/AdsContext';
+import User from '../../models/User.model';
 
 const getAll = async (req: Request, res: Response, next: NextError) => {
   try {
     if (typeof req.query.search === 'string') {
       const requestedAds = await Ad.find({
         title: { $regex: createSearchQuery(req.query.search) },
-      });
+      }).populate<AdData>({ path: 'seller', model: User, select: ['avatar', 'username', 'phone'] });
       if (!requestedAds.length) {
         return next({ status: 404, message: 'Could not find any ads data.' });
       }
       return res.json(requestedAds);
     }
-    const allAds = await Ad.find({});
+    const allAds = await Ad.find({}).populate<AdData>({
+      path: 'seller',
+      model: User,
+      select: ['avatar', 'username', 'phone'],
+    });
     if (!allAds.length) return next({ status: 404, message: 'Could not find any ads data.' });
     return res.json(allAds);
   } catch {
--- server/controllers/ads/getById.ts
+++ server/controllers/ads/getById.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import { NextError } from '../../../interfaces/NextError';
 import Ad from '../../models/Ad.model';
+import { AdData } from '../../../interfaces/AdsContext';
+import User from '../../models/User.model';
 
 const getById = async (req: Request, res: Response, next: NextError) => {
   try {
     const requestedAdId = req.params.id;
-    const requestedAd = await Ad.findById(requestedAdId);
+    const requestedAd = await Ad.findById(requestedAdId).populate<AdData>({
+      path: 'seller',
+      model: User,
+      select: ['avatar', 'username', 'phone'],
+    });
     if (!requestedAd) {
       return next({ status: 404, message: `Ad with id: ${requestedAdId} does not exist!` });
     }
