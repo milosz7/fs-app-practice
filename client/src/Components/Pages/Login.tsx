dee7dd2e9a5f83177b7450544c38c8efa1808a2d
Milosz B <miloszbargiel@gmail.com>
2022-09-15 17:51:04 +0200

    Add LoginForm component and display it on the Login page

--- client/src/Components/Pages/Login.tsx
+++ client/src/Components/Pages/Login.tsx
@@ -1,5 +1,13 @@
+import LoginForm from "../Features/LoginForm";
+import CenteringBox from "../Common/CenteringBox";
+import Typography from "@mui/material/Typography"
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <CenteringBox>
+      <LoginForm />
+    </CenteringBox>
+  )
 };
 
 export default Login;
