5c06743a00afb5b64b60721cf66e165b65b51186
Milosz B <miloszbargiel@gmail.com>
2022-09-15 15:03:26 +0200

    Add fetchAdData helper function

---
+++ client/src/utils/fetchAdData.ts
@@ -0,0 +1,14 @@
+import { AdData } from "../../../interfaces/AdsContext";
+
+const fetchAdData = async () => {
+  const adsDataResp = await fetch('/api/ads', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const adsData: AdData[] = await adsDataResp.json();
+  return adsData;
+};
+
+export default fetchAdData;
