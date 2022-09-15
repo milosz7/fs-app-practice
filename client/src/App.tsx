4572ce16e3a18b9c0f1926623c125538affdef32
Milosz B <miloszbargiel@gmail.com>
2022-09-15 16:10:30 +0200

    Add ad data fetching function and local ad data manipulation functions in the app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,41 @@
 import Navigation from './Components/Views/Navigation/Navigation';
 import Container from '@mui/material/Container';
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { AdData } from '../../interfaces/AdsContext';
+import fetchAdData from './utils/fetchAdData';
+import AdsContext from './Context/AdsContext';
 
 const App = () => {
+  const [ads, setAds] = useState<AdData[] | []>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const adsData = await fetchAdData();
+      setAds(adsData);
+    };
+    fetch();
+  }, []);
+
+  const removeAd = (id: string) => {
+    setAds(ads.filter((ad) => id !== ad._id.toString()));
+  };
+
+  const addNewAd = (adData: AdData) => {
+    setAds([...ads, adData]);
+  };
+
+  const editAd = (id: string, data: AdData) => {
+    setAds(ads.map((ad) => (ad._id.toString() === id ? { ad, ...data } : ad)));
+  };
+
   return (
-    <>
+    <AdsContext.Provider value={{ ads, removeAd, addNewAd, editAd }}>
       <Navigation />
       <Container>
         <Outlet />
       </Container>
-    </>
+    </AdsContext.Provider>
   );
 };
 
