1ac2a063ad5deeb29dfb93b840cad4b22e0e6c7d
Milosz B <miloszbargiel@gmail.com>
2022-09-17 17:34:27 +0200

    Add LogoutDialog functionality

--- client/src/Components/Views/LogoutDialog.tsx
+++ client/src/Components/Views/LogoutDialog.tsx
@@ -7,6 +7,10 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogTransition from '../Features/DialogTransition';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/system';
+import { useContext, useState } from 'react';
+import AuthContext from '../../Context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { FormHelperText } from '@mui/material';
 
 const LogoutDialog = ({
   open,
@@ -15,28 +19,51 @@ const LogoutDialog = ({
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const { logout } = useContext(AuthContext)!;
+  const navigate = useNavigate();
+  const [error, setError] = useState(' ');
+
   const theme = useTheme();
   const shouldBeBigger = useMediaQuery(theme.breakpoints.up('sm'));
+  
+  const manageLogout = async () => {
+    try {
+      const message = await logout();
+      if (message) {
+        return setError(message);
+      }
+      navigate('/');
+      setOpen(false);
+    } catch {
+      setError('Failed to connect with the server.')
+    }
+  }
+
+  const handleClose = () => {
+    setError(' ');
+    setOpen(false)
+  }
 
   return (
     <Dialog
       open={open}
       TransitionComponent={DialogTransition}
       keepMounted
-      onClose={() => setOpen(false)}
+      onClose={() => handleClose()}
     >
-      <DialogContent sx={{ py: 4 }}>
+      <DialogContent sx={{ pt: 4, pb: 1 }}>
         <DialogTitle sx={{ pt: 0 }} textAlign="center" fontSize={shouldBeBigger ? 20 : 18} >
           Are you sure you want to sign out?
         </DialogTitle>
         <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button size={shouldBeBigger ? 'medium' : 'small'} variant="contained" color="success">
+          <Button onClick={() => manageLogout()} size={shouldBeBigger ? 'medium' : 'small'} variant="contained" color="success">
             sign out
           </Button>
-          <Button size={shouldBeBigger ? 'medium' : 'small'} variant="contained" color="error">
+          <Button onClick={() => handleClose()} size={shouldBeBigger ? 'medium' : 'small'} variant="contained" color="error">
             cancel
           </Button>
         </DialogActions>
+        <FormHelperText sx={{textAlign: 'center', py: 1 }}>{error}</FormHelperText>
       </DialogContent>
     </Dialog>
   );
