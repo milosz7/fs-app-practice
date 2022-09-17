5162af1662586b41684da87beabc8d9a3ab46b1f
Milosz B <miloszbargiel@gmail.com>
2022-09-17 17:05:17 +0200

    Fix SideMenu incorrect DOM tree nesting

--- client/src/Components/Views/Navigation/SideMenu.tsx
+++ client/src/Components/Views/Navigation/SideMenu.tsx
@@ -1,30 +1,35 @@
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider'
+import Divider from '@mui/material/Divider';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { Dispatch, SetStateAction } from 'react';
-import { Link as RouterLink} from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-const SideMenu = ({elements, isOpen, setIsOpen}: {elements: {text: string, path: string, icon: JSX.Element}[], isOpen: boolean, setIsOpen: Dispatch<SetStateAction<boolean>>}) => {
-
+const SideMenu = ({
+  elements,
+  isOpen,
+  setIsOpen,
+}: {
+  elements: { text: string; path: string; icon: JSX.Element }[];
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
   const user = true;
 
   return (
     <Drawer anchor="left" open={isOpen} onClose={() => setIsOpen(false)}>
-      <Box sx={{p: 2, display: 'flex', alignItems: 'center'}}>
-        <ShoppingBasketIcon sx={{mr: 3, color: 'action.active'}} fontSize="medium" />
-      <Typography sx={{ display: 'inline-block'}}
-        variant="h6"
-      >
-        Marketplace
-      </Typography>
+      <Box sx={{ p: 2, display: 'flex', alignItems: 'center' }}>
+        <ShoppingBasketIcon sx={{ mr: 3, color: 'action.active' }} fontSize="medium" />
+        <Typography sx={{ display: 'inline-block' }} variant="h6">
+          Marketplace
+        </Typography>
       </Box>
       <Divider />
       <List sx={{ width: 250 }}>
@@ -37,9 +42,7 @@ const SideMenu = ({elements, isOpen, setIsOpen}: {elements: {text: string, path:
               to={path}
             >
               <ListItemIcon>{icon}</ListItemIcon>
-              <Typography textTransform="capitalize">
-                <ListItemText>{text}</ListItemText>
-              </Typography>
+              <ListItemText sx={{ textTransform: 'capitalize' }}>{text}</ListItemText>
             </ListItemButton>
           </ListItem>
         ))}
@@ -53,14 +56,14 @@ const SideMenu = ({elements, isOpen, setIsOpen}: {elements: {text: string, path:
             <ListItemIcon>
               <AccountCircleIcon />
             </ListItemIcon>
-            <Typography textTransform="capitalize">
-              <ListItemText>{user ? 'logout' : 'login'}</ListItemText>
-            </Typography>
+            <ListItemText sx={{ textTransform: 'capitalize' }}>
+              {user ? 'logout' : 'login'}
+            </ListItemText>
           </ListItemButton>
         </ListItem>
       </List>
     </Drawer>
-  )
+  );
 };
 
-export default SideMenu
+export default SideMenu;
