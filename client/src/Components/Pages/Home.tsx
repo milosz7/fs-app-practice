3756c23e5e13522aafb916ee386fff99c4362dda
Milosz B <miloszbargiel@gmail.com>
2022-09-19 19:21:32 +0200

    Add display of the search phrase in the home page title

--- client/src/Components/Pages/Home.tsx
+++ client/src/Components/Pages/Home.tsx
@@ -1,11 +1,19 @@
-import AdsList from "../Features/AdsList";
+import AdsList from '../Features/AdsList';
+import { useLocation } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const Home = () => {
+  const { state }: { state: { query: string | null } } = useLocation();
+  const pageTitle = state && state.query ? `Showing results for: ${state.query}` : 'Avaliable ads';
+
   return (
-    <>
-      <h1>home</h1>
+    <Box pt={3}>
+      <Typography mb={2} fontWeight={700} variant="h5">
+        {pageTitle}
+      </Typography>
       <AdsList />
-    </>
+    </Box>
   );
 };
 
--- client/src/Components/Views/Navigation/SearchBar.tsx
+++ client/src/Components/Views/Navigation/SearchBar.tsx
@@ -6,20 +6,24 @@ import SearchBarContainer from './SearchBarContainer';
 import SearchIconWrapper from './SearchIconWrapper';
 import AdsContext from '../../../Context/AdsContext';
 import ErrorsContext from '../../../Context/ErrorsContext';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+  const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState('');
   const { fetchAdsToState } = useContext(AdsContext)!;
   const { setErrorMessage, setDisplayError } = useContext(ErrorsContext)!;
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
-    e.preventDefault()
-      const output = await fetchAdsToState(searchQuery);
-      if (output) {
-        setErrorMessage(output);
-        setDisplayError(true)
-      }
+    e.preventDefault();
+    const output = await fetchAdsToState(searchQuery);
+    if (output) {
+      setErrorMessage(output);
+      setDisplayError(true);
+      return null;
     }
+    navigate('/', { state: { query: searchQuery } });
+  };
 
   return (
     <SearchBarContainer onSubmit={(e) => handleSubmit(e)}>
