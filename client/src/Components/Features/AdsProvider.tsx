de7b72fb6dc77f546003ec7155afb2af2d0ef320
Milosz B <miloszbargiel@gmail.com>
2022-09-19 19:58:23 +0200

    Fix a bug when fetchAdsToState would return an error object

--- client/src/Components/Features/AdsProvider.tsx
+++ client/src/Components/Features/AdsProvider.tsx
@@ -1,17 +1,24 @@
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode, useState, useEffect, useContext } from 'react';
 import fetchAdData from '../../utils/fetchAdData';
 import AdsContext, { AdData } from '../../Context/AdsContext';
+import ErrorsContext from '../../Context/ErrorsContext';
 
 const AdsProvider = ({ children }: { children: ReactNode }) => {
+  const { setErrorMessage, setDisplayError } = useContext(ErrorsContext)!;
+
   const fetchAdsToState = async (query: string = '') => {
     try {
       const adsData = await fetchAdData(query);
-      setAds(adsData);
+      if (Array.isArray(adsData)) return setAds(adsData);
+      setAds([])
+      setErrorMessage(adsData.message)
+      setDisplayError(true);
     } catch {
-      return 'Failed to connect with the server.'
+      setErrorMessage('Failed to connect with the server.');
+      setDisplayError(true);
     }
   };
-  
+
   useEffect(() => {
     fetchAdsToState();
   }, []);
