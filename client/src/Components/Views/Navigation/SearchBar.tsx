52d5fddf6493c52fe51b83e39de020a1feeff649
Milosz B <miloszbargiel@gmail.com>
2022-09-19 18:10:15 +0200

    Add ad searching functionality

--- client/src/Components/Views/Navigation/SearchBar.tsx
+++ client/src/Components/Views/Navigation/SearchBar.tsx
@@ -1,19 +1,28 @@
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
-import Alert from '@mui/material/Alert';
-import { useRef, useState } from 'react';
+import { useState, useContext } from 'react';
 import CancelIcon from '@mui/icons-material/Cancel';
 import SearchBarInput from './SearchBarInput';
 import SearchBarContainer from './SearchBarContainer';
 import SearchIconWrapper from './SearchIconWrapper';
+import AdsContext from '../../../Context/AdsContext';
+import ErrorsContext from '../../../Context/ErrorsContext';
 
-const SearchBar = ({}) => {
-  // const searchQuery = useRef<null | string>(null);
+const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const { fetchAdsToState } = useContext(AdsContext)!;
+  const { setErrorMessage, setDisplayError } = useContext(ErrorsContext)!;
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+      const output = await fetchAdsToState(searchQuery);
+      if (output) {
+        setErrorMessage(output);
+        setDisplayError(true)
+      }
+    }
 
   return (
-    <SearchBarContainer>
+    <SearchBarContainer onSubmit={(e) => handleSubmit(e)}>
       <SearchIconWrapper>
         <SearchIcon />
       </SearchIconWrapper>
@@ -23,7 +32,8 @@ const SearchBar = ({}) => {
         placeholder="Search.."
       />
       {searchQuery && (
-        <CancelIcon onClick={() => setSearchQuery('')}
+        <CancelIcon
+          onClick={() => setSearchQuery('')}
           sx={{ position: 'absolute', right: 8, top: '50%', transform: 'translateY(-50%)' }}
         />
       )}
