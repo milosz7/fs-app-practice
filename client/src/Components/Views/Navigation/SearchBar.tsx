48474f247e3e467e4eb4147bb1cfbdd0d6518ecf
Milosz B <miloszbargiel@gmail.com>
2022-09-19 15:04:41 +0200

    Extract child components from a searchbar component to prevent unnecessary rerenders

--- client/src/Components/Views/Navigation/SearchBar.tsx
+++ client/src/Components/Views/Navigation/SearchBar.tsx
@@ -1,57 +1,34 @@
-import {styled, alpha} from '@mui/material/styles';
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from '@mui/icons-material/Search'
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import Alert from '@mui/material/Alert';
+import { useRef, useState } from 'react';
+import CancelIcon from '@mui/icons-material/Cancel';
+import SearchBarInput from './SearchBarInput';
+import SearchBarContainer from './SearchBarContainer';
+import SearchIconWrapper from './SearchIconWrapper';
 
-const SearchBar = () => {
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
+const SearchBar = ({}) => {
+  // const searchQuery = useRef<null | string>(null);
+  const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <Search>
+    <SearchBarContainer>
       <SearchIconWrapper>
         <SearchIcon />
       </SearchIconWrapper>
-      <StyledInputBase placeholder='Search..' />
-    </Search>
-  )
-}
+      <SearchBarInput
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search.."
+      />
+      {searchQuery && (
+        <CancelIcon onClick={() => setSearchQuery('')}
+          sx={{ position: 'absolute', right: 8, top: '50%', transform: 'translateY(-50%)' }}
+        />
+      )}
+    </SearchBarContainer>
+  );
+};
 
 export default SearchBar;
---
+++ client/src/Components/Views/Navigation/SearchBarInput.tsx
@@ -0,0 +1,20 @@
+import { styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+const SearchBarInput = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default SearchBarInput;
