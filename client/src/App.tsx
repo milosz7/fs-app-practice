e7c945230422b10e7cfc190cfe16cce90f8f267b
Milosz B <miloszbargiel@gmail.com>
2022-09-20 14:11:57 +0200

    Include LoadingProvider and DisplayContent components in the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
 import Navigation from './Components/Views/Navigation/Navigation';
-import Container from '@mui/material/Container';
-import { Outlet } from 'react-router-dom';
 import AdsProvider from './Components/Features/AdsProvider';
 import AuthProvider from './Components/Features/AuthProvider';
 import ErrorsProvider from './Components/Features/ErrorsProvider';
 import ErrorAlert from './Components/Views/ErrorAlert';
+import DisplayContent from './Components/Utility/DisplayContent';
+import LoadingProvider from './Components/Features/LoadingProvider';
 
 const App = () => {
   return (
-    <ErrorsProvider>
-      <AuthProvider>
-        <AdsProvider>
-          <ErrorAlert />
-          <Navigation />
-          <Container>
-            <Outlet />
-          </Container>
-        </AdsProvider>
-      </AuthProvider>
-    </ErrorsProvider>
+    <LoadingProvider>
+      <ErrorsProvider>
+        <AuthProvider>
+          <AdsProvider>
+            <ErrorAlert />
+            <Navigation />
+            <DisplayContent />
+          </AdsProvider>
+        </AuthProvider>
+      </ErrorsProvider>
+    </LoadingProvider>
   );
 };
 
