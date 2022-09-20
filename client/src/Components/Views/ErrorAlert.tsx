28539a1f0b175dc14dd45140303f1d09ddc95a94
Milosz B <miloszbargiel@gmail.com>
2022-09-20 13:03:52 +0200

    Change the layout of ErrorAlert component

--- client/src/Components/Views/ErrorAlert.tsx
+++ client/src/Components/Views/ErrorAlert.tsx
@@ -1,22 +1,39 @@
-import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ErrorsContext from '../../Context/ErrorsContext';
-import { errorAlertDisplayDurationInMs } from '../../constants';
+import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/system';
+import Zoom from '@mui/material/Zoom';
+import Box from '@mui/material/Box';
 
 const ErrorAlert = () => {
-  const { displayError, errorMessage, closeError } = useContext(ErrorsContext)!;
+  const { displayError, errorMessage, closeError, autoCloseError } = useContext(ErrorsContext)!;
+  const theme = useTheme();
+  const isMedium = useMediaQuery(theme.breakpoints.up('sm'));
+  
+  useEffect(() => {
+    if (displayError) {
+      autoCloseError();
+    }
+  }, [displayError, autoCloseError])
 
   return (
-    <Snackbar
-      open={displayError}
-      onClose={closeError}
-      autoHideDuration={errorAlertDisplayDurationInMs}
-    >
-      <Alert sx={{'& .MuiAlert-icon': {
-        alignItems: 'center'
-      }}} onClose={closeError} severity="error">{errorMessage}</Alert>
-    </Snackbar>
+    <Box top={20} left="50%" sx={{transform:"translateX(-50%)", width: 400, maxWidth: '90vw'}} position="fixed" zIndex={999}>
+      <Zoom in={displayError}>
+      <Alert
+        sx={{
+          '& .MuiAlert-icon': {
+            alignItems: 'center',
+          },
+        }}
+        onClose={closeError}
+        severity="error"
+      >
+        <Typography variant={isMedium ? 'body1' : 'body2'}>{errorMessage}</Typography>
+      </Alert>
+      </Zoom>
+    </Box>
   );
 };
 
