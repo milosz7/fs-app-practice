844ef0bd559baa8ece17c931fe195e6e99cd89da
Milosz B <miloszbargiel@gmail.com>
2022-09-20 14:08:51 +0200

    Include ProgressBar height in CenteringBox component

--- client/src/Components/Common/CenteringBox.tsx
+++ client/src/Components/Common/CenteringBox.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 
 const CenteringBox = ({ children }: { children: React.ReactNode }) => {
   const navigationHeightInPx = 64;
+  const loadingBarHeigthInPx = 4;
 
   return (
     <Box
@@ -11,7 +12,7 @@ const CenteringBox = ({ children }: { children: React.ReactNode }) => {
       alignItems="center"
       flexDirection="column"
       flexWrap="nowrap"
-      height={`calc(100vh - ${navigationHeightInPx}px)`}
+      height={`calc(100vh - ${navigationHeightInPx + loadingBarHeigthInPx}px)`}
     >
       {children}
     </Box>
