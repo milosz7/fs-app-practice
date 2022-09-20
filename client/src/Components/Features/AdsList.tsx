69fd39fbe7df884a10c57962c7db517fc9ece5fc
Milosz B <miloszbargiel@gmail.com>
2022-09-20 14:13:13 +0200

    Add loading state management in AdsList and Home components

--- client/src/Components/Features/AdsList.tsx
+++ client/src/Components/Features/AdsList.tsx
@@ -2,20 +2,29 @@ import Grid from '@mui/material/Grid';
 import { useContext } from 'react';
 import AdsContext from '../../Context/AdsContext';
 import AdCard from '../Views/AdCard';
+import NotFoundError from '../Views/NotFoundError';
+import LoadingContext from '../../Context/LoadingContext';
 
 const AdsList = () => {
   const { ads } = useContext(AdsContext)!;
+  const { loading } = useContext(LoadingContext)!;
+
+  if (loading) return null
+
+  if (!ads.length) {
+    return (
+      <NotFoundError
+        title="Something went wrong!"
+        message="We could not find any ads mathcing your criteria."
+      />
+    );
+  }
 
   return (
     <Grid container spacing={2}>
-      {ads.map(({_id, image, title, location }) => (
+      {ads.map(({ _id, image, title, location }) => (
         <Grid item key={_id.toString()} xs={12} sm={6} md={4} xl={3}>
-          <AdCard
-            id={_id}
-            image={image}
-            title={title}
-            location={location}
-          ></AdCard>
+          <AdCard id={_id} image={image} title={title} location={location}></AdCard>
         </Grid>
       ))}
     </Grid>
--- client/src/Components/Pages/Home.tsx
+++ client/src/Components/Pages/Home.tsx
@@ -2,16 +2,23 @@ import AdsList from '../Features/AdsList';
 import { useLocation } from 'react-router-dom';
 import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+import { useContext } from 'react';
+import AdsContext from '../../Context/AdsContext';
+import LoadingContext from '../../Context/LoadingContext';
 
 const Home = () => {
   const { state }: { state: { query: string | null } } = useLocation();
   const pageTitle = state && state.query ? `Showing results for: ${state.query}` : 'Avaliable ads';
+  const { ads } = useContext(AdsContext)!;
+  const { loading } = useContext(LoadingContext)!;
 
   return (
     <Box pt={3}>
-      <Typography mb={2} fontWeight={700} variant="h5">
-        {pageTitle}
-      </Typography>
+      {(ads.length !== 0 && !loading) && (
+        <Typography mb={2} fontWeight={700} variant="h5">
+          {pageTitle}
+        </Typography>
+      )}
       <AdsList />
     </Box>
   );
