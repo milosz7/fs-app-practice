246227964192ece71dcb04cf80137d2377f08f22
Milosz B <miloszbargiel@gmail.com>
2022-09-20 14:16:24 +0200

    Move context providers to a separate folder, update imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import Navigation from './Components/Views/Navigation/Navigation';
-import AdsProvider from './Components/Features/AdsProvider';
-import AuthProvider from './Components/Features/AuthProvider';
-import ErrorsProvider from './Components/Features/ErrorsProvider';
+import AdsProvider from './Components/Providers/AdsProvider';
+import AuthProvider from './Components/Providers/AuthProvider';
+import ErrorsProvider from './Components/Providers/ErrorsProvider';
 import ErrorAlert from './Components/Views/ErrorAlert';
 import DisplayContent from './Components/Utility/DisplayContent';
-import LoadingProvider from './Components/Features/LoadingProvider';
+import LoadingProvider from './Components/Providers/LoadingProvider';
 
 const App = () => {
   return (
