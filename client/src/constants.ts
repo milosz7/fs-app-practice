ab55bb90674fa9bd3e4169fe7ae49d12f0c5ca9f
Milosz B <miloszbargiel@gmail.com>
2022-09-21 11:53:00 +0200

    Add defaultAdImage path and defaultAdValue constants

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,10 @@
 export const transitionFinishDelayInMs = 500;
 export const dataUpdateDelayInMs = 150;
 export const errorAlertDisplayDurationInMs = 3000;
+export const defaultAdImagePath = '/no-image.jpg';
+export const defaultAdValue = {
+  title: '',
+  location: '',
+  price: 0,
+  description: '',
+};
