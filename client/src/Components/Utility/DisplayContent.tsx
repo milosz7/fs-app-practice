20b11e89c4356487c6c6129484e8aba166f70f50
Milosz B <miloszbargiel@gmail.com>
2022-09-22 22:55:13 +0200

    Add usePersistLogin hook in DisplayContent utility component

--- client/src/Components/Utility/DisplayContent.tsx
+++ client/src/Components/Utility/DisplayContent.tsx
@@ -1,18 +1,31 @@
 import { Outlet } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import LoadingContext from '../../Context/LoadingContext';
 import LinearProgress from '@mui/material/LinearProgress';
 import Container from '@mui/material/Container';
+import usePersistLogin from '../../Hooks/usePersistLogin';
+import AuthContext from '../../Context/AuthContext';
 
 const DisplayContent = () => {
   const { loading } = useContext(LoadingContext)!;
+  const { setUser, user } = useContext(AuthContext)!;
+
+  const isLogged = usePersistLogin();
+  useEffect(() => {
+    if (!user) {
+      const fetchSessionData = async () => {
+        setUser(await isLogged());
+      };
+      fetchSessionData();
+    }
+  }, [user, isLogged, setUser]);
 
   return (
     <>
       <LinearProgress sx={{ visibility: loading ? 'visible' : 'hidden' }} color="secondary" />
-        <Container>
-          <Outlet />
-        </Container>
+      <Container>
+        <Outlet />
+      </Container>
     </>
   );
 };
