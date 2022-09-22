1958f09440b011b27c1996fe4fe3113cd2ef06b7
Milosz B <miloszbargiel@gmail.com>
2022-09-22 23:00:41 +0200

    Add an Ad page view

--- client/src/Components/Pages/Add.tsx
+++ client/src/Components/Pages/Add.tsx
@@ -1,5 +1,20 @@
+import AdDataManipulationForm from '../Features/AdDataManipulationForm';
+import AdPreview from '../Features/AdPreview';
+import { useContext } from 'react';
+import LocalAdDataContext from '../../Context/LocalAdDataContext';
+import Box from '@mui/material/Box';
+import declareImgPath from '../../utils/declareImgPath';
+import { defaultAdImagePath } from '../../constants';
+
 const Add = () => {
-  return <h1>Add</h1>;
+  const { localAdData, adFile } = useContext(LocalAdDataContext)!;
+
+  return (
+    <Box>
+      <AdDataManipulationForm />
+      <AdPreview previewData={localAdData} image={adFile ? adFile : declareImgPath(defaultAdImagePath)} />
+    </Box>
+  );
 };
 
 export default Add;
