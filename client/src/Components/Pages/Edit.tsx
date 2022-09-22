c40938fd3297e9e564e60a1d187f889e76861b27
Milosz B <miloszbargiel@gmail.com>
2022-09-22 23:00:57 +0200

    Add an Edit page view

--- client/src/Components/Pages/Edit.tsx
+++ client/src/Components/Pages/Edit.tsx
@@ -1,5 +1,35 @@
+import Box from '@mui/system/Box';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import AdsContext from '../../Context/AdsContext';
+import NotFoundError from '../Views/NotFoundError';
+import AdDataManipulationForm from '../Features/AdDataManipulationForm';
+import AdPreview from '../Features/AdPreview';
+import LocalAdDataContext from '../../Context/LocalAdDataContext';
+import declareImgPath from '../../utils/declareImgPath';
+
 const Edit = () => {
-  return <h1>Edit</h1>;
+  const { id } = useParams();
+  const { ads } = useContext(AdsContext)!;
+  const adData = ads.find(({ _id }) => id === _id.toString());
+  const { adFile } = useContext(LocalAdDataContext)!;
+
+  if (!adData)
+    return (
+      <NotFoundError
+        title="Something went wrong!"
+        message={`Ad data for id: ${id} was not found.`}
+      />
+    );
+
+  const { _id, title, description, location, price, image } = adData;
+
+  return (
+    <Box mt={3}>
+      <AdDataManipulationForm data={{ _id, title, description, price, location }} />
+      <AdPreview image={adFile ? adFile : declareImgPath(image)} previewData={{title, location, description, price}} />
+    </Box>
+  );
 };
 
 export default Edit;
