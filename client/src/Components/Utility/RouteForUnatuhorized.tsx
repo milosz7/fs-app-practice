b9dccdb158e4bcd1b4b606077320652e549ba0b1
Milosz B <miloszbargiel@gmail.com>
2022-09-23 14:44:28 +0200

    Fix a bug where an error message would be displayed after succesful authorization

--- client/src/Components/Utility/RouteForUnatuhorized.tsx
+++ client/src/Components/Utility/RouteForUnatuhorized.tsx
@@ -2,19 +2,21 @@ import { useContext, ReactElement, useEffect } from "react";
 import AuthContext from "../../Context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import AlertsContext from "../../Context/AlertsContext";
+import LoadingContext from "../../Context/LoadingContext";
 
 const RouteForUnauthorized = ({ children }: {children: ReactElement<any, any>} ) => {
   const { user } = useContext(AuthContext)!;
   const { setDisplayedMessage, setMessageDisplay } = useContext(AlertsContext)!;
-  const navigate = useNavigate()
+  const { loading } = useContext(LoadingContext)!;
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(user) {
+    if(user && !loading) {
       navigate('/');
       setDisplayedMessage('You are already logged in.');
       setMessageDisplay(true);
     }
-  },[navigate, user, setMessageDisplay, setDisplayedMessage])
+  },[navigate, user, setMessageDisplay, setDisplayedMessage, loading])
 
   return children; 
 }
