a989b7d36b0c4e735197fe98bf596ef849b16769
Milosz B <miloszbargiel@gmail.com>
2022-09-23 23:23:41 +0200

    Remove doubled error display while visiting login page

--- client/src/Components/Utility/ProtectedRoute.tsx
+++ client/src/Components/Utility/ProtectedRoute.tsx
@@ -1,22 +1,18 @@
 import { useContext, ReactElement, useEffect } from "react";
 import AuthContext from "../../Context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import AlertsContext from "../../Context/AlertsContext";
 import LoadingContext from "../../Context/LoadingContext";
 
 const ProtectedRoute = ({ children }: {children: ReactElement<any, any>} ) => {
   const { user } = useContext(AuthContext)!;
-  const { setDisplayedMessage, setMessageDisplay } = useContext(AlertsContext)!;
   const navigate = useNavigate()
   const { loading } = useContext(LoadingContext)!;
 
   useEffect(() => {
     if(!user && !loading) {
       navigate('/login');
-      setDisplayedMessage('You have to be logged in!');
-      setMessageDisplay(true);
     }
-  },[navigate, user, setMessageDisplay, setDisplayedMessage, loading])
+  },[navigate, user, loading])
 
   return children; 
 }
