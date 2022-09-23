4aa9d56f5c51fd213e91ff42ee2f7650af1d1979
Milosz B <miloszbargiel@gmail.com>
2022-09-23 12:47:51 +0200

    Wrap getUserData in a useCallback so it does not get recreated without purpose

--- client/src/Hooks/usePersistLogin.ts
+++ client/src/Hooks/usePersistLogin.ts
@@ -1,35 +1,40 @@
 import mongoose from 'mongoose';
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import AlertsContext from '../Context/AlertsContext';
 import LoadingContext from '../Context/LoadingContext';
+import AuthContext from '../Context/AuthContext';
 
 const usePersistLogin = () => {
   const { setDisplayedMessage, setMessageDisplay } = useContext(AlertsContext)!;
   const { setLoading } = useContext(LoadingContext)!;
-  const getUserData = async () => {
-  try {
-    setLoading(true)
-    const response = await fetch('/auth/is-logged', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const status = response.status;
-    if (status === 200) {
-      const userData: { username: string; id: mongoose.Types.ObjectId } | null = await response.json();
-      setLoading(false)
-      return userData;
+  const { user } = useContext(AuthContext)!;
+
+  const getUserData = useCallback(async () => {
+    if (!user) {
+      try {
+        setLoading(true)
+        const response = await fetch('/auth/is-logged', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const status = response.status;
+        if (status === 200) {
+          const userData: { username: string; id: mongoose.Types.ObjectId } | null = await response.json();
+          setLoading(false)
+          return userData;
+        }
+        setLoading(false)
+        return null
+      } catch {
+        setLoading(false)
+        setDisplayedMessage('Failed to fetch session data.');
+        setMessageDisplay(true)
+        return null
+      }
     }
-    setLoading(false)
-    return null
-  } catch {
-    setLoading(false)
-    setDisplayedMessage('Failed to fetch session data.');
-    setMessageDisplay(true)
-    return null
-  }
-  };
+  }, [user, setLoading, setDisplayedMessage, setMessageDisplay])
   return getUserData;
 };
 
