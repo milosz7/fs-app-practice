db1f4850c5022ab1766272c635c48e85de15d4b5
Milosz B <miloszbargiel@gmail.com>
2022-09-24 19:45:58 +0200

    Add fetchAdsToState call after deleting Ad Data

--- client/src/Hooks/useDeleteData.ts
+++ client/src/Hooks/useDeleteData.ts
@@ -2,10 +2,12 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoadingContext from '../Context/LoadingContext';
 import AlertsContext from '../Context/AlertsContext';
+import AdsContext from '../Context/AdsContext';
 
 const useDeleteAdData = () => {
   const { setLoading } = useContext(LoadingContext)!;
   const { displayError, displaySuccess } = useContext(AlertsContext)!;
+  const { fetchAdsToState } = useContext(AdsContext)!;
   const navigate = useNavigate();
   const deleteData = async (endpoint: string, redirect: string) => {
     try {
@@ -20,6 +22,7 @@ const useDeleteAdData = () => {
 
       if (status === 200) {
         displaySuccess(message);
+        fetchAdsToState('');
         setLoading(false);
         navigate(redirect);
       }
