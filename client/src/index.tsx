ff716dcb44a3e92a49b9fce93dde23a0d13274b6
Milosz B <miloszbargiel@gmail.com>
2022-10-01 18:33:45 +0200

    Extract router from index file, add Profile page base file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,69 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
-import Home from './Components/Pages/Home';
-import Login from './Components/Pages/Login';
-import Edit from './Components/Pages/Edit';
-import Ad from './Components/Pages/Ad';
-import Add from './Components/Pages/Add';
-import Register from './Components/Pages/Register';
-import NotFound from './Components/Pages/NotFound';
-import ProtectedRoute from './Components/Utility/ProtectedRoute';
-import RouteForUnauthorized from './Components/Utility/RouteForUnatuhorized';
-import AdDataProvider from './Components/Providers/LocalAdDataProvider';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="login"
-        element={
-          <RouteForUnauthorized>
-            <Login />
-          </RouteForUnauthorized>
-        }
-      />
-      <Route
-        path="edit/:id"
-        element={
-          <ProtectedRoute>
-            <AdDataProvider>
-              <Edit />
-            </AdDataProvider>
-          </ProtectedRoute>
-        }
-      />
-      <Route path="ad/:id" element={<Ad />} />
-      <Route
-        path="add"
-        element={
-          <ProtectedRoute>
-            <AdDataProvider>
-              <Add />
-            </AdDataProvider>
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="register"
-        element={
-          <RouteForUnauthorized>
-            <Register />
-          </RouteForUnauthorized>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+import router from './router';
+import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
---
+++ client/src/router.tsx
@@ -0,0 +1,67 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Login from './Components/Pages/Login';
+import Edit from './Components/Pages/Edit';
+import Ad from './Components/Pages/Ad';
+import Add from './Components/Pages/Add';
+import Register from './Components/Pages/Register';
+import NotFound from './Components/Pages/NotFound';
+import ProtectedRoute from './Components/Utility/ProtectedRoute';
+import RouteForUnauthorized from './Components/Utility/RouteForUnatuhorized';
+import AdDataProvider from './Components/Providers/LocalAdDataProvider';
+import Profile from './Components/Pages/Profile';
+import App from './App';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="login"
+        element={
+          <RouteForUnauthorized>
+            <Login />
+          </RouteForUnauthorized>
+        }
+      />
+      <Route
+        path="edit/:id"
+        element={
+          <ProtectedRoute>
+            <AdDataProvider>
+              <Edit />
+            </AdDataProvider>
+          </ProtectedRoute>
+        }
+      />
+      <Route path="ad/:id" element={<Ad />} />
+      <Route
+        path="add"
+        element={
+          <ProtectedRoute>
+            <AdDataProvider>
+              <Add />
+            </AdDataProvider>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="register"
+        element={
+          <RouteForUnauthorized>
+            <Register />
+          </RouteForUnauthorized>
+        }
+      />
+      <Route path="profile/:name" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+export default router;
