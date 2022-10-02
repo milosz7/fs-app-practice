7460d256112a620d4c710d7f79331b3db5ce0942
Milosz B <miloszbargiel@gmail.com>
2022-10-02 19:00:06 +0200

    Use extracted AdBase components to update AdPreview component

--- client/src/Components/Features/AdPreview.tsx
+++ client/src/Components/Features/AdPreview.tsx
@@ -2,7 +2,13 @@ import { useEffect, useState, useRef } from 'react';
 import AdBase from '../Common/AdBase';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import useFetchUserData from '../../Hooks/useFetchUserData';
+import AdContactContainer from '../Common/AdDisplay/AdContactContainer';
+import AdUserDataContainer from '../Common/AdDisplay/AdUserDataContainer';
+import AdDescriptionContainer from '../Common/AdDisplay/AdDescriptionContainer';
+import AdImageContainer from '../Common/AdDisplay/AdImageContainer';
+import AdInfoContainer from '../Common/AdDisplay/AdInfoContainer';
 
 const AdPreview = ({
   previewData,
@@ -22,11 +28,11 @@ const AdPreview = ({
     phone: string;
   } | null>(null);
   const previewPublishedDate = useRef<string>();
-  const fetchUserData = useFetchUserData()
+  const fetchUserData = useFetchUserData();
 
   useEffect(() => {
     const dispatchFetchUser = async () => {
-      setUserData(await fetchUserData())
+      setUserData(await fetchUserData());
     };
     dispatchFetchUser();
     previewPublishedDate.current = new Date().toISOString();
@@ -34,17 +40,36 @@ const AdPreview = ({
 
   if (!userData) return null;
 
+  const { title, location, description, price } = previewData;
+
   return (
     <Box mt={3}>
       <Typography mb={2} fontWeight={700} variant="h5">
         Ad preview
       </Typography>
-      <AdBase
-        published={previewPublishedDate.current!}
-        {...previewData}
-        image={image}
-        seller={userData}
-      />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7} borderRadius={3}>
+          <AdImageContainer pathOrFile={image} altText={title} />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Box height="100%" display="flex" flexDirection="column">
+            <AdUserDataContainer
+              username={userData.username}
+              avatar={userData.avatar}
+            />
+            <AdInfoContainer
+              title={title}
+              location={location}
+              price={price}
+              published={previewPublishedDate.current!}
+            />
+            <AdContactContainer phone={userData.phone} />
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <AdDescriptionContainer description={description} />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
