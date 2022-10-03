673a62b971431548e0ac522c620330ef5150b6ed
Milosz B <miloszbargiel@gmail.com>
2022-10-03 22:09:26 +0200

    Replace getAdsByUsername controller with getAdsByUserId

--- server/controllers/ads/getAdsByUserId.ts
+++ server/controllers/ads/getAdsByUserId.ts
@@ -4,13 +4,10 @@ import Ad from '../../models/Ad.model';
 import User from '../../models/User.model';
 import { PopulatedAdData } from '../../interfaces/PopulatedAdData';
 
-const getByUsername = async (req: Request, res: Response, next: NextError) => {
+const getAdsByUserId = async (req: Request, res: Response, next: NextError) => {
   try {
-    const username = req.params.username;
-    const userData = await User.findOne({ username: { $eq: username } }).collation({
-      locale: 'en_US',
-      strength: 2,
-    });
+    const id = req.params.id;
+    const userData = await User.findById(id);
     if (!userData) return next({ status: 404, message: 'User not found.' });
     const userAds = await Ad.find({ seller: { $eq: userData._id } })
       .sort({ _id: -1 })
@@ -19,12 +16,10 @@ const getByUsername = async (req: Request, res: Response, next: NextError) => {
         model: User,
         select: ['avatar', 'username', 'phone'],
       });
-    if (!userAds.length)
-      return next({ status: 200, message: 'There are no ads posted by this user avaliable.' });
     return res.json(userAds);
   } catch {
     return next({ status: 500, message: 'Internal server error.' });
   }
 };
 
-export default getByUsername;
+export default getAdsByUserId;
--- server/routes/ads.routes.ts
+++ server/routes/ads.routes.ts
@@ -6,13 +6,13 @@ import getById from '../controllers/ads/getById';
 import addNew from '../controllers/ads/addNew';
 import deleteAd from '../controllers/ads/delete';
 import edit from '../controllers/ads/edit';
-import getByUsername from '../controllers/ads/getByUsername';
+import getAdsByUserId from '../controllers/ads/getAdsByUserId';
 
 const router = express.Router();
 
 router.route('/ads').get(getAll);
 
-router.route('/ads/user/:username').get(getByUsername);
+router.route('/ads/user/:id').get(getAdsByUserId);
 
 router.route('/ads/:id').get(getById);
 
